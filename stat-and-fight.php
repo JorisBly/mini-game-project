e4a3d91b1a83e4ff97f74e5764f9ba4f3fb423fe
joris <joris.bailly@jobtrek.ch>
2022-01-07 08:39:14 +0100

    Ajout des stats de personnage et début de logique

--- stat-and-fight.php
+++ stat-and-fight.php
@@ -1,1 +1,46 @@
 <?php
+
+
+while ($me['pv'] == 0 && $ennemy ['pv'] == 0) {
+    $me = [
+        'pv' => 30,
+        'defense' => 10,
+        'spell' => [
+            'fire-ball' => 10,
+            'sword-dash' => 5,
+            'regain' => 7,
+        ]
+    ];
+
+    $ennemy = [
+        'pv' => 30,
+        'defense' => 5,
+        'spell' => [
+            'sword dash' => 5,
+            'regain' => 7,
+        ]
+    ];
+
+    $action = (int)readline('Choose a spell:(fire-ball, sword-dash,regain )');
+
+}
+
+
+function myStatAfterRound($attack, $myStat, $oppenants)
+{
+
+
+    if ($attack == $myStat ['spell']['fire-ball']) {
+        return $oppenants['pv'] -= ['spell']['fire-ball'];
+    } elseif ($attack == $myStat ['spell']['sword-dash']) {
+        return $oppenants['pv'] -= $myStat ['spell']['sword-dash'];
+    } elseif ($attack == $myStat ['spell']['regain']) {
+        return $myStat['pv'] += $myStat ['spell']['regain'];
+    }
+    return $myStat;
+}
+
+function ennemyStatAfterRound()
+{
+
+}
